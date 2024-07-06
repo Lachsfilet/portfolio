3951de7fc5e5b6ceb0aef249d0f960193b3a0c81
Fynn Berger <lachsfilet.coder@gmail.com>
2024-07-06 14:09:41 +0200

    css improvements + added bouncing arrow

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -14,42 +14,48 @@ export default function LandingPage() {
       easing: "ease-out-cubic",
     });
   }, []);
+
   return (
-    <main className="min-h-screen px-4 max-w-full flex justify-center items-center">
-      <section className="flex w-full items-center justify-center">
-        <div data-aos="fade-up">
-          <h1 className="text-7xl font-bold tracking-tight max-w-max">
-            Hej, I'm Fynn
-          </h1>
-          <p className="mt-4 text-2xl text-slate-400/50 max-w-max">
-            a self taught web developer.
-          </p>
+    <main className="min-h-screen px-4 max-w-full relative overflow-x-hidden">
+      <div className="min-h-screen px-4 max-w-full flex justify-center items-center">
+        <section className="flex w-full items-center justify-center">
+          <div data-aos="fade-up">
+            <h1 className="text-7xl font-bold tracking-tight max-w-max">
+              Hej, I'm Fynn
+            </h1>
+            <p className="mt-4 text-2xl text-slate-400/50 max-w-max">
+              a self taught web developer.
+            </p>
 
-          <div className="mt-4 flex">
-            <Link href={"https://www.linkedin.com/in/fynn-berger-17b209318/"}>
-              <img
-                src="https://skillicons.dev/icons?i=linkedin"
-                alt="LinkedIn"
-              />
-            </Link>
-            <Link href={"https://github.com/Lachsfilet"} className="ml-2">
-              <img src="https://skillicons.dev/icons?i=github" alt="GitHub" />
-            </Link>
+            <div className="mt-4 flex">
+              <Link href={"https://www.linkedin.com/in/fynn-berger-17b209318/"}>
+                <img
+                  src="https://skillicons.dev/icons?i=linkedin"
+                  alt="LinkedIn"
+                />
+              </Link>
+              <Link href={"https://github.com/Lachsfilet"} className="ml-2">
+                <img src="https://skillicons.dev/icons?i=github" alt="GitHub" />
+              </Link>
+            </div>
           </div>
-        </div>
 
-        <Image
-          src="/pfp.jpg"
-          alt="Fynn's Profile Picture"
-          width={250}
-          height={250}
-          layout="intrinsic"
-          className="ml-[30vw] object-cover object-center select-none w-52 h-52 md:w-72 md:h-72 rounded-full overflow-hidden"
-          loading="eager"
-          priority
-          data-aos="fade-left"
-        />
-      </section>
+          <Image
+            src="/pfp.jpg"
+            alt="Fynn's Profile Picture"
+            width={250}
+            height={250}
+            layout="intrinsic"
+            className="ml-[30vw] object-cover object-center select-none w-52 h-52 md:w-72 md:h-72 rounded-full overflow-hidden"
+            loading="eager"
+            priority
+            data-aos="fade-left"
+          />
+        </section>
+      </div>
+      <div className="absolute bottom-10 w-full flex justify-center">
+        <div className="animate-bounce text-3xl">&darr;</div>
+      </div>
     </main>
   );
 }
