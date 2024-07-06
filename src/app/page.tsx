2bc77e8aa30e94c399d7b50580bbeaa274d426d6
Fynn Berger <lachsfilet.coder@gmail.com>
2024-07-06 11:40:20 +0200

    REMAKE: Landing Hej section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,40 +1,7 @@
 "use client";
 
-import Image from "next/image";
-import "aos/dist/aos.css";
-import { useEffect } from "react";
-import AOS from "aos";
+import LandingPage from "@/components/LandingPage";
 
 export default function Page() {
-  useEffect(() => {
-    AOS.init({
-      disable: "phone",
-      duration: 800,
-      easing: "ease-out-cubic",
-    });
-  }, []);
-  return (
-    <main className="flex flex-col items-center min-h-screen mx-auto max-w-4xl px-4 py-20">
-      <section className="flex w-full items-center" data-aos="fade-up">
-        <Image
-          src="/pfp.jpg"
-          alt="Fynn's Profile Picture"
-          width={250}
-          height={250}
-          layout="intrinsic"
-          className="mr-6 object-cover object-center select-none w-24 h-24 md:w-32 md:h-32 rounded-full overflow-hidden"
-          loading="eager"
-          priority
-        />
-        <div>
-          <h1 className="text-5xl font-semibold tracking-tight max-w-max">
-            Hej, I'm Fynn
-          </h1>
-          <p className="mt-4 text-lg text-slate-400/50 max-w-max">
-            Web Developer & <br /> Tech Enthusiast
-          </p>
-        </div>
-      </section>
-    </main>
-  );
+  return <LandingPage />;
 }
--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -1,29 +1,42 @@
+"use client";
+
 import Image from "next/image";
+import "aos/dist/aos.css";
+import { useEffect } from "react";
+import AOS from "aos";
 
 export default function LandingPage() {
+  useEffect(() => {
+    AOS.init({
+      disable: "phone",
+      duration: 800,
+      easing: "ease-out-cubic",
+    });
+  }, []);
   return (
-    <div className="flex justify-between">
-      <div>
-        <h1 className="text-4xl max-w-max font-semibold tracking-tight">
-          Hey, I&apos;m Fynn
-        </h1>
-        <p className="mt-4 text-lg max-w-max text-white/50">
-          a web developer <br /> while developing some of my own.
-        </p>
-      </div>
+    <main className="min-h-screen px-4 py-40 max-w-full overflow-hidden">
+      <section className="flex w-full items-center justify-center">
+        <div data-aos="fade-up">
+          <h1 className="text-7xl font-bold tracking-tight max-w-max">
+            Hej, I'm Fynn
+          </h1>
+          <p className="mt-4 text-2xl text-slate-400/50 max-w-max">
+            Web Developer & <br /> Tech Enthusiast
+          </p>
+        </div>
 
-      <div className="w-24 h-24 md:w-32 md:h-32 rounded-full overflow-hidden hidden md:block mr-6">
         <Image
-          src="/pfp.png"
-          alt="Hugo"
+          src="/pfp.jpg"
+          alt="Fynn's Profile Picture"
           width={250}
           height={250}
           layout="intrinsic"
-          className="object-cover object-center select-none w-full h-full"
+          className="ml-[30vw] object-cover object-center select-none w-52 h-52 md:w-72 md:h-72 rounded-full overflow-hidden"
           loading="eager"
           priority
+          data-aos="fade-left"
         />
-      </div>
-    </div>
+      </section>
+    </main>
   );
 }
